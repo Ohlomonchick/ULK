c409d38a8f99dabebbdd522dfa4626be97dd7c9d
Ohlomonchick <proskurin.dima16@gmail.com>
2024-03-31 18:37:13 +0300

    Postgres support is on

--- Cyberpolygon/settings.py
+++ Cyberpolygon/settings.py
@@ -84,24 +84,24 @@ WSGI_APPLICATION = 'Cyberpolygon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if not os.environ.get('USE_POSTGRES', ''):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
-# UNCOMMENT TO ACTIVATE POSTGRES
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': os.environ.get('DB_USER', 'postgres'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-#         'HOST': os.environ.get('DB_PASSWORD', 'localhost'),
-#         'PORT': '5432',
-#     }
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': os.environ.get('DB_USER', 'postgres'),
+            'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
+            'HOST': os.environ.get('DB_PASSWORD', 'claba'),
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
