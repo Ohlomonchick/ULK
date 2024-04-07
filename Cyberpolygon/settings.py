dfa793a6e31915bb0f70f5a317a77e1d45056f60
Ohlomonchick <proskurin.dima16@gmail.com>
2024-04-07 11:32:52 +0300

    Перемещение наших статических файлов в ассеты

--- Cyberpolygon/settings.py
+++ Cyberpolygon/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,6 +141,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ('assets',)
 
 STATICFILES_FINDERS = [
   # First add the two default Finders, since this will overwrite the default.
