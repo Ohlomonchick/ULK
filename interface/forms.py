0874c6610195171b43123491919789e2078164bc
Ohlomonchick <proskurin.dima16@gmail.com>
2024-04-16 23:07:01 +0300

    Опциональный ввод пароля в админке. Вывод ошибок при регистрации.

--- interface/forms.py
+++ interface/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import User
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 
 class LabAnswerForm(forms.Form):
     answer_flag = forms.CharField(label="Флаг:", widget=forms.TextInput(attrs={'class': 'input', 'type': 'text'}))
@@ -11,6 +14,39 @@ class SignUpForm(forms.ModelForm):
         fields = ["first_name", "last_name", "platoon"]
         model = User
 
+
 class ChangePasswordForm(forms.Form):
     password1 = forms.CharField(widget=forms.PasswordInput(), label = " Новый пароль")
     password2 = forms.CharField(widget=forms.PasswordInput(), label = "Повторите пароль")
+
+
+class CustomUserCreationForm(UserCreationForm):
+    password1 = forms.CharField(label='Пароль', required=False, widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', required=False, widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].required = False
+        self.fields['password2'].required = False
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "platoon")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get("password1")
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Пароли не совпадают")
+
+        return password2
