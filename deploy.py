1f92e4603531022e9f65a233ec024d0353c70c90
Ohlomonchick <proskurin.dima16@gmail.com>
2024-10-06 21:05:54 +0300

    fixed nginx static hosting

--- deploy.py
+++ deploy.py
@@ -5,8 +5,12 @@ with open('nginx.conf.template') as f:
     string = f.read()
     string = string.replace('{%media%}', os.path.abspath('media'))
     string = string.replace('{%static%}', os.path.abspath('static'))
-    # ip = sys.argv[1]
-    # string = string.replace('{%ip%}', ip)
+
+    ip = '192.168.100.10:80'
+    if len(sys.argv) > 1:
+        ip = sys.argv[1]
+    string = string.replace('{%ip%}', ip)
+
     with open('nginx.conf', mode='w', encoding='utf-8') as write_f:
         write_f.write(string)
 
--- run_service.sh
+++ run_service.sh
@@ -4,7 +4,10 @@ python3 deploy.py
 
 sudo rm -rf /etc/systemd/system/cyberpolygon.service
 sudo cp cyberpolygon.service /etc/systemd/system/cyberpolygon.service
-sudo chmod 111 /etc/systemd/system/cyberpolygon.service
+sudo chmod 444 /etc/systemd/system/cyberpolygon.service
+
+sudo chmod -R 777 media
+sudo chmod -R 777 static
 
 sudo systemctl daemon-reload
 sudo systemctl enable cyberpolygon
