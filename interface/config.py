b5e1a318fa43b8d011d6b9b75ec09de2cbb33b3d
Ohlomonchick <proskurin.dima16@gmail.com>
2024-11-24 23:02:43 +0300

    variate pnet dir on debug

--- interface/config.py
+++ interface/config.py
@@ -1,4 +1,9 @@
 import os
+from django.conf import settings
 
 PNET_URL = 'http://172.18.4.160'
-PNET_BASE_DIR = os.environ.get('PNET_BASE_DIR', '/Practice work/Test_Labs/api_test_dir')
+
+if settings.DEBUG:
+    PNET_BASE_DIR = '/Practice work/Test_Labs/api_test_dir'
+else:
+    PNET_BASE_DIR = os.environ.get('PNET_BASE_DIR', '/Practice work/Test_Labs/api_test_dir')
