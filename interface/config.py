c754c5d572d24e72457cea35306a70fce68285fd
Ohlomonchick <proskurin.dima16@gmail.com>
2025-08-17 21:01:43 +0300

    make pnetlab more generic

--- interface/config.py
+++ interface/config.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import time
+from functools import wraps
 from django.conf import settings
 
 if 'makemigrations' not in sys.argv and 'migrate' not in sys.argv:
@@ -9,10 +11,38 @@ else:
         return default
 
 
+def cache_for_minutes(minutes):
+    """Simple cache decorator that caches function results for specified minutes"""
+    def decorator(func):
+        cache = {}
+        
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # Create a cache key based on function name and arguments
+            cache_key = f"{func.__name__}:{hash(str(args) + str(sorted(kwargs.items())))}"
+            current_time = time.time()
+            
+            # Check if we have a cached result and it's still valid
+            if cache_key in cache:
+                cached_result, cached_time = cache[cache_key]
+                if current_time - cached_time < minutes * 60:  # Convert minutes to seconds
+                    return cached_result
+            
+            # If not cached or expired, call the function and cache the result
+            result = func(*args, **kwargs)
+            cache[cache_key] = (result, current_time)
+            return result
+        
+        return wrapper
+    return decorator
+
+
+@cache_for_minutes(1)
 def get_pnet_url():
     return get_config('PNET_URL', 'http://172.18.4.160')
 
 
+@cache_for_minutes(1)
 def get_pnet_base_dir():
     if settings.DEBUG:
         return get_config('PNET_BASE_DIR', '/Practice work/Test_Labs/api_test_dir')
--- interface/utils.py
+++ interface/utils.py
@@ -2,3 +2,7 @@ import hashlib
 
 def get_pnet_password(user_password):
     return hashlib.md5((user_password + '42').encode()).hexdigest()[:8]
+
+
+def get_pnet_lab_name(lab):
+    return lab.slug + '_' + lab.lab_type.lower()
