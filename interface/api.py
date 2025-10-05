143dce7359cde396ccca45543b1d494a730430ed
Ohlomonchick <64347118+Ohlomonchick@users.noreply.github.com>
2025-10-05 13:02:43 +0300

    fixed Kkz is not defined

--- interface/api.py
+++ interface/api.py
@@ -21,7 +21,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_POST
 
 
-from .models import Competition, LabLevel, Lab, LabTask, Answers, User, TeamCompetition, LabTasksType
+from .models import Competition, LabLevel, Lab, LabTask, Answers, User, TeamCompetition, LabTasksType, Kkz
 from .serializers import LabLevelSerializer, LabTaskSerializer
 from .api_utils import get_issue
 from .config import get_pnet_base_dir, get_pnet_url, get_web_url
