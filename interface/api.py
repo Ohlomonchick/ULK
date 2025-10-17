014920b3fa65accc3f2ed8af69ebaaa8436deebd
Ohlomonchick <proskurin.dima16@gmail.com>
2025-10-17 17:07:20 +0300

    fix imports

--- interface/api.py
+++ interface/api.py
@@ -21,8 +21,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_POST
 
 
-from .models import Competition, LabLevel, Lab, LabTask, Answers, User, TeamCompetition, LabTasksType, Kkz, Platoon, \
-    LabType, KkzPreview, KkzPreview, KkzPreview
+from .models import Competition, LabLevel, Lab, LabTask, Answers, User, TeamCompetition, LabTasksType, Kkz, Platoon, LabType, KkzPreview
 from .serializers import LabLevelSerializer, LabTaskSerializer
 from .api_utils import get_issue
 from .config import get_pnet_base_dir, get_pnet_url, get_web_url
