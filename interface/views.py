576d1e2261c315c0153eb194ea9d7ced7d41f0dc
Ohlomonchick <proskurin.dima16@gmail.com>
2026-02-15 13:33:27 +0300

    Update views.py

--- interface/views.py
+++ interface/views.py
@@ -901,7 +901,7 @@ def kibana_dashboard(request, slug):
         dash = "78289c40-86da-11e8-b59d-21efb914e65c-ecs"
     else:
         dash = "ccdaf445-a62a-4fb0-9300-5b2cd38c687e"
-    dashboard_url = get_kibana_url() + f"/app/dashboards#/view/{dash}?_g=(filters:!(),refreshInterval:(pause:!t,value:60000),time:(from:now-1d%2Fd,to:now))&_a=(filters:!(),query:(language:kuery,query:'_index:{competition.lab.slug}-{request.user.pnet_login}*'))"
+    dashboard_url = get_kibana_url() + f"/app/dashboards#/view/{dash}?_g=(filters:!(),refreshInterval:(pause:!t,value:60000),time:(from:now-1d,to:now))&_a=(filters:!(),query:(language:kuery,query:'_index:{competition.lab.slug}-{request.user.pnet_login}*'))"
 
     return render(request, 'interface/kibana_dashboard.html', {'dashboard_url': dashboard_url, 'slug': slug})
 
